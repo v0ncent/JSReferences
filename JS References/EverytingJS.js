d944a44bcc6237ff0312c86564223125df06b26a
v0ncent <banksvincent5@gmail.com>
2022-03-02 16:25:53 -0600

    FirstCommit(inprog)

---
+++ JS References/EverytingJS.js
@@ -0,0 +1,32 @@
+//----comments----
+
+// in line comment same as java
+
+/* multi line comment
+same as java */
+
+
+//----data types and variables----
+/* data types can be any of the below
+undefined, null, boolean, string, symbol, number, and object */
+
+var myName = "vincent"; /* var just means variable and can be used throghout
+your whole program */
+
+//there are three ways to define data types in JS
+let home = "House"; //let, can only be used in the scope of where you declared it
+
+const lastName = "Banks"; //const, never changes
+
+
+//----Storing Values with Assignment Operator----
+var a; //declare variable to be assigned later
+var b = 2; //assign variable
+
+a=7; //we can assign the var a here 
+
+b=a; //we can also assign one variable to another so now b is equal to the contents of a
+
+console.log(a); //console.log loggs whatever you ask it to into the console
+
+
